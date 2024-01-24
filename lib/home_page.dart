85f3d51ce647c503008031a4a448f7134dc0cb2d
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-24 14:27:45 -0500

    Remove floating action buttons into another page and access cubit through provider

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,24 +3,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'cubit/counter_cubit.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final counterCubit = CounterCubit();
 
   @override
   Widget build(BuildContext context) {
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -39,28 +34,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 }),
           ],
         ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              counterCubit.increment();
-            },
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              counterCubit.decrement();
-            },
-            tooltip: 'Decrement',
-            child: const Icon(Icons.minimize),
-          ),
-        ],
       ),
     );
   }
